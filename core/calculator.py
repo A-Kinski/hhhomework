74dea04eacd0d23e974dd5872f28cbbe943fc35f
Anton <asidorov90@gmail.com>
2020-01-15 21:12:13 +0300

    calculator is done

--- core/calculator.py
+++ core/calculator.py
@@ -4,12 +4,33 @@ def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    operationsDict = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '/': lambda x, y: x / y,
+        '*': lambda x, y: x * y,
+        '**': lambda x, y: x ** y
+    }
+    return operationsDict[operation](a, b)
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    aString = input('Введите первое число: ')
+    while not aString.isdigit():
+        print('Это не число')
+        aString = input('Введите первое число: ')
+    a = int(aString)
+
+    bString = input('Введите второе число: ')
+    while not bString.isdigit():
+        print('Это не число')
+        bString = input('Введите второе число: ')
+    b = int(bString)
+
+    operationsList = ['+', '-', '/', '*', '**']
+    operation = input('Введите операцию: ')
+    while not operation in operationsList:
+        print('Операция ' + operation + ' не поддерживается')
+        operation = input('Введите операцию: ')
 
     print('Результат: ', calculator(a, b, operation))
